4ebb00d17049ffa5970f4926d1f056ffe4dee3bd
huibunny <cc@huibunny.com>
2022-08-03 07:55:30 +0800

    +verfy token

--- server/server_context.go
+++ server/server_context.go
@@ -1,10 +1,35 @@
 package server
 
 import (
+	"bytes"
+	"errors"
+
 	"github.com/valyala/fasthttp"
 	"github.com/wanghongfei/gogate/conf"
 	"github.com/wanghongfei/gogate/utils"
 )
+
+func VerifyToken(ctx *fasthttp.RequestCtx, secret string) error {
+	// Get the Basic Authentication credentials
+	auth := ctx.Request.Header.Peek("Authorization")
+	strAuth := string(auth)
+	userInfo, err := ParseToken(token, secret)
+	if err != nil {
+		err = errors.New("ParseToken returns error: %v.", err)
+	} else {
+		userName := userInfo["username"]
+		password := userInfo["password"]
+		createTime := userInfo["create_time"]
+		tokenExpire := userInfo["token_expire"]
+		if len(userInfo["username"]) > 0 && len(userInfo["password"]) > 0 {
+			//
+		} else {
+			//
+		}
+	}
+
+	return err
+}
 
 // 从请求上下文中取出*ServiceInfo
 func GetServiceInfoFromUserValue(ctx *fasthttp.RequestCtx, key string) (*conf.ServiceInfo, bool) {
